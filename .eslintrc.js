659d0d4028654b387f129f63c3bc0d3629d834b8
Tjark Kühl <tjark-kuehl@hotmail.com>
2022-02-18 18:34:16 +0100

    refactor: cleanup index file

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: './index.js',
+  rules: {
+    /**
+      * doesnt make sense in this project
+      * because node isnt importing modules
+    * */
+    'unicorn/prefer-module': 'off',
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.json'],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
