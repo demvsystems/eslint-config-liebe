8046f92b21669b3656482638755ed22bb9b1d23b
Tjark Kühl <tjark-kuehl@hotmail.com>
2022-02-18 19:04:19 +0100

    fix(sonarjs): update rules

--- rules/sonarjs.js
+++ rules/sonarjs.js
@@ -16,6 +16,10 @@ module.exports = {
   rules: {
     // we found it to intrusive to instandly rewrite a method
     // https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/cognitive-complexity.md
-    'sonarjs/cognitive-complexity': 'warn',
+    'sonarjs/cognitive-complexity': ['warn', 20],
+
+    // make it less of a chore and more like a proposal to change it
+    // https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/no-duplicate-string.md
+    'sonarjs/no-duplicate-string': 'warn',
   },
 };
