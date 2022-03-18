5cb6a03f6df40b663d402fdd72307fc3e8bd0a65
Tjark Kühl <tjark-kuehl@hotmail.com>
2022-03-18 10:50:02 +0100

    fix(import): ignore ts definition files

--- rules/import.js
+++ rules/import.js
@@ -59,5 +59,14 @@ module.exports = {
         'import/no-default-export': 'off',
       },
     },
+    {
+      files: ['*.d.ts'],
+      rules: {
+        // Reports if a module's default export is unnamed
+        // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-anonymous-default-export.md
+        'import/no-anonymous-default-export': 'off',
+        'import/no-default-export': 'off',
+      },
+    },
   ],
 };
