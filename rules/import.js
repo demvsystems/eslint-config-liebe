2044e3fb8ec3dc44f8c76512942ef7e16d4265c6
Tjark Kühl <tjark-kuehl@hotmail.com>
2022-03-18 10:37:04 +0100

    fix(import): dont expect default export in .vue

--- rules/import.js
+++ rules/import.js
@@ -54,8 +54,9 @@ module.exports = {
       files: ['*.vue'],
       rules: {
         // Reports if a module's default export is unnamed
-        // https://github.com/benmosher/eslint-plugin-import/blob/d9b712ac7fd1fddc391f7b234827925c160d956f/docs/rules/no-anonymous-default-export.md
+        // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-anonymous-default-export.md
         'import/no-anonymous-default-export': 'off',
+        'import/no-default-export': 'off',
       },
     },
   ],
