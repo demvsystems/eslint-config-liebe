a510b3d4d270eaf2e8ee86580acf3aac2e4f2ef4
Jonathan Wischhusen <j.wischhusen@demv.de>
2023-05-10 11:03:04 +0200

    feat: force consistent type imports

--- packages/typescript/configs/typescriptEslint.js
+++ packages/typescript/configs/typescriptEslint.js
@@ -38,6 +38,7 @@ module.exports = {
         functions: false, classes: true, variables: true, typedefs: true,
       },
     ],
+    '@typescript-eslint/consistent-type-imports': 'error',
   },
   overrides: [
     {
